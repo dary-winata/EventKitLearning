7801c77a4a4a787065309e4b58db2466af4edea5
dary-winata <dary.winata15@gmail.com>
2023-07-25 16:40:23 +0700

    Initial Commit

---
+++ Eventkit-testing/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Eventkit-testing
+//
+//  Created by dary winata nugraha djati on 25/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Eventkit-testing/Eventkit_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eventkit_testingApp.swift
+//  Eventkit-testing
+//
+//  Created by dary winata nugraha djati on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Eventkit_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
